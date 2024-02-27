b084943bf6e5c4b08fa1ad8d31d6c6941f6b9c6b
Fábio Oliveira <fabio20rj@gmail.com>
2024-02-27 11:04:48 +0100

    feat: create model movie for store movies in db

--- api/models.py
+++ api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+
+    title = models.CharField(max_length=200)
+    year = models.CharField(max_length=10)
+    rated = models.CharField(max_length=100)
+    released = models.CharField(max_length=50)
+    runtime = models.CharField(max_length=50)
+    genre = models.CharField(max_length=100)
+    director = models.CharField(max_length=200)
+    writer = models.CharField(max_length=200)
+    actors = models.TextField()
+    plot = models.TextField()
+    language = models.CharField(max_length=200)
+    country = models.CharField(max_length=100)
+    awards = models.CharField(max_length=200)
+    poster = models.TextField()
+    ratings = models.JSONField()
+    metascore = models.CharField(max_length=20)
+    imdbrating = models.CharField(max_length=10)
+    imdbvotes = models.CharField(max_length=50)
+    imdbid = models.CharField(max_length=20)
+    type = models.CharField(max_length=10)
+    dvd = models.CharField(max_length=50)
+    boxoffice = models.CharField(max_length=200)
+    production = models.CharField(max_length=100)
+    website = models.TextField()
+    response = models.CharField(max_length=10)
