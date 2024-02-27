99e0b378b6ed8275582d08150d56c9f3173e0711
Fábio Oliveira <fabio20rj@gmail.com>
2024-02-27 12:41:18 +0100

    feat: create a service for fetch movie in omdb

---
+++ api/services/fetch_movie.py
@@ -0,0 +1,16 @@
+import httpx
+from django.conf import settings
+
+OMDB_API_KEY = settings.OMDB_API_KEY
+
+
+def get_imdb_id_movie_list() -> list:
+    with open("movies") as file:
+        movies = file.read().split("\n")[:-1]
+    return movies
+
+
+def get_movie(imdb_number: str = None, title: str = None):
+    params = {"apikey": OMDB_API_KEY, "i": imdb_number, "t": title}
+    response = httpx.get("https://www.omdbapi.com/", params=params)
+    return response
