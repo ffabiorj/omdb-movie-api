cdc0fe3719d43b08e3777c9393a87546ae7fd727
Fábio Oliveira <fabio20rj@gmail.com>
2024-03-09 18:58:11 +0100

    feat: add view for populate database with movies

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from api import views as view
 
 urlpatterns = [
-    path("movies", view.GetMovies.as_view(), name="get-movies"),
-    path("movie", view.GetMovie.as_view(), name="get-movie"),
-    path("add-movie", view.AddMovie.as_view(), name="add-movie"),
-    path("delete-movie", view.DeleteMovie.as_view(), name="delete-movie"),
+    path(
+        "populate-database/", view.PopulateDatabase.as_view(), name="populate-database"
+    ),
+    path("movies/", view.GetMovies.as_view(), name="get-movies"),
+    path("movie/", view.GetMovie.as_view(), name="get-movie"),
+    path("add-movie/", view.AddMovie.as_view(), name="add-movie"),
+    path("delete-movie/", view.DeleteMovie.as_view(), name="delete-movie"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from django.core.paginator import EmptyPage, Paginator
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
@@ -6,6 +8,57 @@ from rest_framework.views import APIView
 
 from api.models import Movie
 from api.movie_serializer import IdSerializer, MovieSerializer, TitleSerializer
+from api.services.fetch_movie import get_imdb_id_movie_list, get_movie
+
+
+class PopulateDatabase(APIView):
+    """A class view for populate database with 100 mvoies"""
+
+    def get(self, request):
+        movies = Movie.objects.count()
+        if movies >= 100:
+            return Response("The database already has 100 movies")
+        quantity = 0
+        movie_id_list = get_imdb_id_movie_list()
+        while quantity < 100:
+            id_movie = movie_id_list[randint(0, 99)]
+            try:
+                movie = get_movie(id_movie)
+            except Exception:
+                continue
+            movie = movie.json()
+            movie_dict = {
+                "title": movie.get("Title"),
+                "year": movie.get("Year"),
+                "rated": movie.get("Rated"),
+                "released": movie.get("Released"),
+                "runtime": movie.get("Runtime"),
+                "genre": movie.get("Genre"),
+                "director": movie.get("Director"),
+                "writer": movie.get("Writer"),
+                "actors": movie.get("Actors"),
+                "plot": movie.get("Plot"),
+                "language": movie.get("Language"),
+                "country": movie.get("Country"),
+                "awards": movie.get("Awards"),
+                "poster": movie.get("Poster"),
+                "ratings": movie.get("Ratings"),
+                "metascore": movie.get("Metascore"),
+                "imdbrating": movie.get("imdbRating"),
+                "imdbvotes": movie.get("imdbVotes"),
+                "imdbid": movie.get("imdbID"),
+                "type": movie.get("Type"),
+                "dvd": movie.get("DVD"),
+                "boxoffice": movie.get("BoxOffice"),
+                "production": movie.get("Production"),
+                "website": movie.get("Website"),
+                "response": movie.get("Response"),
+            }
+            movie_serializer = MovieSerializer(data=movie_dict)
+            movie_serializer.is_valid(raise_exception=True)
+            movie_serializer.save()
+            quantity += 1
+        return Response("Insertion complete", status=status.HTTP_201_CREATED)
 
 
 class GetMovies(APIView):
