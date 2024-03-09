2440efe212c021b423a75f000dbbb0187a58728c
Fábio Oliveira <fabio20rj@gmail.com>
2024-03-09 18:58:41 +0100

    test: create tests for populate view

---
+++ api/tests/__init__.py
@@ -0,0 +1,2 @@
+from .test_fetch_movie import FetchMovieTestCase, GetListTitleImdbTestCase
+from .test_movies import MovieTestCase
--- api/tests/test_fetch_movie.py
+++ api/tests/test_fetch_movie.py
@@ -1,12 +1,15 @@
 import json
 from unittest.mock import patch
 
+import httpx
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 from api.services.fetch_movie import get_imdb_id_movie_list, get_movie
 
 
-class GetListTitleimdbTestCase(APITestCase):
+class GetListTitleImdbTestCase(APITestCase):
 
     def test_sould_return_a_list_ids(self):
         self.assertTrue(get_imdb_id_movie_list())
@@ -14,6 +17,7 @@ class GetListTitleimdbTestCase(APITestCase):
 
 
 class FetchMovieTestCase(APITestCase):
+
     def setUp(self):
         with open("/home/fabio/workspace/omdb-movie-api/api/tests/movie.json") as file:
             self.movie = file.read()
@@ -40,3 +44,17 @@ class FetchMovieTestCase(APITestCase):
         self.assertEqual(response["Runtime"], runtime)
         self.assertEqual(response["Genre"], genre)
         self.assertEqual(response["Director"], director)
+
+    @patch("httpx.get")
+    def test_populate_database(self, mocke_movie):
+        mocke_movie.return_value = httpx.Response(200, json=json.loads(self.movie))
+        url = reverse("populate-database")
+        response = self.client.get(url)
+        self.assertEqual(response.data, "Insertion complete")
+
+    @patch("api.models.Movie.objects.count")
+    def test_should_response_already_has_100_movies(self, mock_value):
+        mock_value.return_value = 100
+        url = reverse("populate-database")
+        response = self.client.get(url)
+        self.assertEqual(response.data, "The database already has 100 movies")
