0dcc03f41f53b5d9e52b9b7fc809e87c8cb1b62a
Fábio Oliveira <fabio20rj@gmail.com>
2024-03-12 21:55:02 +0100

    chore: fix warning from env database url

--- omdb_movie_api/settings.py
+++ omdb_movie_api/settings.py
@@ -1,49 +1,50 @@
 import io
+import os
+from pathlib import Path
+from urllib.parse import urlparse
+
 import environ
 from google.cloud import secretmanager
-import os
-from urllib.parse import urlparse
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(DEBUG=(bool, False))
-env_file = os.path.join(BASE_DIR, '.env')
+env_file = os.path.join(BASE_DIR, ".env")
 env.read_env(env_file)
 
 
 if os.path.isfile(env_file):
     # read a local .env file
     env.read_env(env_file)
-elif os.environ.get('GOOGLE_CLOUD_PROJECT', None):
+elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
     # pull .env file from Secret Manager
-    project_id = os.environ.get('GOOGLE_CLOUD_PROJECT')
+    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
 
     client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get('SETTINGS_NAME', 'django_settings')
-    name = f'projects/{project_id}/secrets/{settings_name}/versions/latest'
-    payload = client.access_secret_version(name=name).payload.data.decode('UTF-8')
+    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
 
     env.read_env(io.StringIO(payload))
 else:
-    raise Exception('No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.')
+    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-APPENGINE_URL = env('APPENGINE_URL', default=None)
+APPENGINE_URL = env("APPENGINE_URL", default=None)
 if APPENGINE_URL:
     # ensure a scheme is present in the URL before it's processed.
     if not urlparse(APPENGINE_URL).scheme:
-        APPENGINE_URL = f'https://{APPENGINE_URL}'
+        APPENGINE_URL = f"https://{APPENGINE_URL}"
 
     ALLOWED_HOSTS = [urlparse(APPENGINE_URL).netloc]
     CSRF_TRUSTED_ORIGINS = [APPENGINE_URL]
     SECURE_SSL_REDIRECT = True
 else:
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -98,13 +99,13 @@ WSGI_APPLICATION = "omdb_movie_api.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if env.db():
-    DATABASES = {'default': env.db()}
+if env("DATABASE_URL"):
+    DATABASES = {"default": env.db()}
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 
